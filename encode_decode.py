c66e601d7fc36ec8563093ab02de329d4316a570
Hancir <phancir@lm-sjl-11000481.corp.ebay.com>
2015-08-13 15:35:52 -0700

    making an update to encode_decode.py

--- encode_decode.py
+++ encode_decode.py
@@ -15,6 +15,15 @@ for i in dec:
     print "entry number %d = %s" % (n, i)
     n += 1
 
+delimiter = '---'
+enc = encode(["1", "cba", "dc"], delimiter)
+print "%s is the encoded list" % enc
+
+dec = decode(enc, delimiter)
+print "%s is the decoded list" % dec
+
+
+
 
     
 
